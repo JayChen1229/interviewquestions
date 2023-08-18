7e8f14e0460119188ab154cf924f839a062b114a
CHEN <y2sk951@gmail.com>
2023-08-18 20:10:39 +0800

    initial

---
+++ src/main/java/com/example/engineerpracticequestions/EngineerPracticeQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.engineerpracticequestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineerPracticeQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineerPracticeQuestionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/engineerpracticequestions/EngineerPracticeQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.engineerpracticequestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineerPracticeQuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
