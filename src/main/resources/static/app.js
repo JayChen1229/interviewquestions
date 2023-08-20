538f44dc0805876d4c37606bde947e26a769eec5
CHEN <y2sk951@gmail.com>
2023-08-20 08:11:10 +0800

    feat: Implemented prevention of inputting empty values in data. doc: Added a feature to prevent users from submitting empty values in data input fields. bug: N

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -25,93 +25,105 @@ new Vue({
     },
     methods: {
         register() {
-            let user = {
-                userName: this.userName,
-                email: this.registerEmail,
-                password: this.registerPassword,
+            if (this.userName && this.registerEmail && this.registerPassword) {
+                let user = {
+                    userName: this.userName,
+                    email: this.registerEmail,
+                    password: this.registerPassword,
+                }
+                axios.post('/register', user)
+                    .then(response => {
+                        if (response.data) {
+                            Swal.fire({
+                                position: 'top',
+                                title: 'Registration success',
+                                timer: 1500,
+                                showConfirmButton: false,
+                                background: 'rgba(255, 255, 255, .7)'
+                            });
+                        } else {
+                            Swal.fire({
+
+                                position: 'top',
+                                title: 'This mailbox is already registered',
+                                timer: 1500,
+                                showConfirmButton: false,
+                                background: 'rgba(255, 255, 255, .7)'
+                            });
+                        }
+
+
+                    });
             }
-            axios.post('/register', user)
-                .then(response => {
-                    if (response.data) {
-                        Swal.fire({
-                            position: 'top',
-                            title: 'Registration success',
-                            timer: 1500,
-                            showConfirmButton: false,
-                            background: 'rgba(255, 255, 255, .7)'
-                        });
-                    } else {
-                        Swal.fire({
-
-                            position: 'top',
-                            title: 'This mailbox is already registered',
-                            timer: 1500,
-                            showConfirmButton: false,
-                            background: 'rgba(255, 255, 255, .7)'
-                        });
-                    }
-
-
-                });
         },
         login() {
-            axios.post('/login', null, {
-                params: {
-                    email: this.email,
-                    password: this.password
-                }
-            })
-                .then(response => {
-                    if (response.data) {
-                        // 登入成功，放入user資料
-                        this.user = response.data;
-                        this.user.imgUrl = '/img/users/' + this.user.userId;
-                    } else {
-                        Swal.fire({
-                            position: 'top',
-                            title: 'Login failed',
-                            timer: 1500,
-                            showConfirmButton: false,
-                            background: 'rgba(255, 255, 255, .7)'
-                        });
+            if (this.email && this.password) {
+                axios.post('/login', null, {
+                    params: {
+                        email: this.email,
+                        password: this.password
                     }
+                })
+                    .then(response => {
+                        if (response.data) {
+                            // 登入成功，放入user資料
+                            this.user = response.data;
+                            this.user.imgUrl = '/img/users/' + this.user.userId;
+                        } else {
+                            Swal.fire({
+                                position: 'top',
+                                title: 'Login failed',
+                                timer: 1500,
+                                showConfirmButton: false,
+                                background: 'rgba(255, 255, 255, .7)'
+                            });
+                        }
 
-                });
+                    });
+            }
         },
         logout() {
             // 登出，移除user資料
             this.user = null;
         },
         postPost() {
-            this.newPost.user = this.user;
-            axios.post('/posts', this.newPost)
-                .then(response => {
-                    if (response) {
-                        Swal.fire({
-                            position: 'top',
-                            title: 'Successfully published',
-                            timer: 1500,
-                            showConfirmButton: false,
-                            background: 'rgba(255, 255, 255, .7)'
-                        });
-                        const postId = response.data.postId; // Assuming the response contains the postId
+            if (this.newPost.content) {
+                this.newPost.user = this.user;
+                axios.post('/posts', this.newPost)
+                    .then(response => {
+                        if (response) {
+                            Swal.fire({
+                                position: 'top',
+                                title: 'Successfully published',
+                                timer: 1500,
+                                showConfirmButton: false,
+                                background: 'rgba(255, 255, 255, .7)'
+                            });
+                            const postId = response.data.postId; // Assuming the response contains the postId
 
-                        // Upload image after post is created
-                        this.uploadImage(postId);
-                        this.image = null; // 清空已選擇照片
-                        this.fetchPosts();  // 上傳成功 則刷新文章列表
-                    } else {
-                        Swal.fire({
-                            position: 'top',
-                            title: 'Failed to publish',
-                            timer: 1500,
-                            showConfirmButton: false,
-                            background: 'rgba(255, 255, 255, .7)'
-                        });
-                    }
+                            // Upload image after post is created
+                            this.uploadImage(postId);
+                            this.image = null; // 清空已選擇照片
+                            this.fetchPosts();  // 上傳成功 則刷新文章列表
+                        } else {
+                            Swal.fire({
+                                position: 'top',
+                                title: 'Failed to publish',
+                                timer: 1500,
+                                showConfirmButton: false,
+                                background: 'rgba(255, 255, 255, .7)'
+                            });
+                        }
 
-                });
-
+                    });
+            } else {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Post content cannot be empty',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
             // 重置新文章的內容並隱藏文章文字框
             this.newPost.content = '';
             this.showTextarea = false;
@@ -128,7 +140,7 @@ new Vue({
 
         uploadImage(postId) {
             // 如果圖片存在
-            if(this.image){
+            if (this.image) {
                 const formData = new FormData();
                 formData.append('image', this.image);
                 axios.post(`/posts/${postId}/upload`, formData, {
@@ -273,46 +285,49 @@ new Vue({
         // 添加留言
         addComment(post) {
             Swal.fire({
-                title: 'Edit Content',
+                title: 'Add Comment',
                 input: 'textarea',
-                // inputValue: this.comment.content,
                 showCancelButton: true,
                 confirmButtonText: 'Save',
                 cancelButtonText: 'Cancel',
             }).then((result) => {
                 if (result.isConfirmed) {
-                    console.log(result.value);
-                    this.comment.content = result.value;
+                    if (result.value) {
+                        this.comment.content = result.value;
+                        console.log(result.value)
+                        this.comment.user = this.user;
+                        this.comment.post = post;
 
-                    // 在這裡你可以實現保存編輯內容的邏輯
-                    this.comment.user = this.user;
-                    this.comment.post = post;
+                        axios.post('/comments', this.comment)
+                            .then(response => {
+                                if (response) {
+                                    Swal.fire({
+                                        position: 'top',
+                                        title: 'Successfully modified',
+                                        timer: 1500,
+                                        showConfirmButton: false,
+                                        background: 'rgba(255, 255, 255, .7)'
+                                    });
+                                    this.fetchPosts();
+                                } else {
+                                    Swal.fire({
+                                        position: 'top',
+                                        title: 'fail to edit',
+                                        timer: 1500,
+                                        showConfirmButton: false,
+                                        background: 'rgba(255, 255, 255, .7)'
+                                    });
+                                }
 
-                    console.log(this.comment);
-
-
-                    axios.post('/comments', this.comment)
-                        .then(response => {
-                            if (response) {
-                                Swal.fire({
-                                    position: 'top',
-                                    title: 'Successfully modified',
-                                    timer: 1500,
-                                    showConfirmButton: false,
-                                    background: 'rgba(255, 255, 255, .7)'
-                                });
-                                this.fetchPosts();
-                            } else {
-                                Swal.fire({
-                                    position: 'top',
-                                    title: 'fail to edit',
-                                    timer: 1500,
-                                    showConfirmButton: false,
-                                    background: 'rgba(255, 255, 255, .7)'
-                                });
-                            }
-
-                        });
+                            });
+                    }else {
+                        Swal.fire({
+                            icon: 'warning',
+                            title: 'Comment cannot be empty',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    }
                 }
             });
         },
